825a6d834ad701114def046c9d4ec263e2788007
Dario Didi <dididario.dd2@gmail.com>
2025-06-11 11:59:03 +0300

    v-2 add booking and review components

---
+++ interfaces/index.ts
@@ -0,0 +1,48 @@
+export interface CardProps { }
+export interface ButtonProps { }
+/*{
+    name: "Clifftop Retreat",
+    address: {
+      state: "Cape Town",
+      city: "Western Cape",
+      country: "South Africa"
+    },
+    rating: 4.78,
+    category: ["Ocean View", "Private Pool", "Self Checkin"],
+    price: 4100,
+    offers: {
+      bed: "3",
+      shower: "3",
+      occupants: "4-5"
+    },
+    image: "https://example.com/image20.jpg",
+    discount: ""
+  }*/
+export interface PropertyProps {
+  name: string;
+  address: Address;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: Offers
+  image: string;
+  discount: string;
+  //description: string;
+}
+
+export interface Address {
+  state: string;
+  city: string;
+  country: string;
+}
+
+export interface Offers {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+
+
+export interface PillProps {
+  title: string
+}
